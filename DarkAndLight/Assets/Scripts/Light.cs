68fc4798b5d4e3e5243941fb30d50d8044373a4b
sunquan8094 <sunquan8094@Dereks-MacBook-Pro-3.local>
2015-01-24 17:12:32 -1000

    First commit

---
+++ DarkAndLight/Assets/Scripts/Light.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Light : MonoBehaviour {
+	public bool on;
+	public float radius;
+	// Use this for initialization
+	void Start () {
+		on = true;
+		radius = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
